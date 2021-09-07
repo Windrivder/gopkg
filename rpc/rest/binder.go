e83a207704c4c10834893d375f9eeb96488d90e0
Windrivder <windrivder@gmail.com>
2021-09-07 13:20:45 +0800

    feat: binder

--- rpc/rest/binder.go
+++ rpc/rest/binder.go
@@ -1,12 +1,8 @@
 package rest
 
 import (
-	"net/http"
-	"strings"
-
 	ut "github.com/go-playground/universal-translator"
 	"github.com/labstack/echo/v4"
-	"github.com/windrivder/gopkg/encoding/jsonx"
 	"github.com/windrivder/gopkg/i18n"
 	"github.com/windrivder/gopkg/util/valid"
 )
@@ -35,25 +31,7 @@ func NewBinder(locale string) (echo.Binder, error) {
 }
 
 func (b *binder) Bind(i interface{}, c echo.Context) (err error) {
-	if err := b.BindPathParams(c, i); err != nil {
-		return err
-	}
-
-	if c.Request().Method == http.MethodGet || c.Request().Method == http.MethodDelete {
-		if err = b.BindQueryParams(c, i); err != nil {
-			return err
-		}
-	}
-
-	req := c.Request()
-	ctype := req.Header.Get(echo.HeaderContentType)
-	if strings.HasPrefix(ctype, echo.MIMEApplicationJSON) {
-		if err = jsonx.DecodeReader(req.Body, i); err != nil {
-			return err
-		}
-	}
-
-	if err = b.BindBody(c, i); err != nil {
+	if err := b.DefaultBinder.Bind(i, c); err != nil {
 		return err
 	}
 
